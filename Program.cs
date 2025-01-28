0ef03e2afc9e267afcc9510de85a9da131cc5f6b
Hyrum-Kunzler <Hyrum.kunzler@students.snow.edu>
2025-01-28 15:15:22 -0700

    4th Step

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ int c = 2;
 int d = (a + b) / c;
 Console.WriteLine(d);*/
 
-// Step 3
+/* Step 3
 int a = 7;
 int b = 4;
 int c = 3;
@@ -29,4 +29,17 @@ int max = int.MaxValue;
 int min = int.MinValue;
 Console.WriteLine($"The range of integers is {min} to {max}");
 int what = max + 3;
-Console.WriteLine($"An example of overflow: {what}");
+Console.WriteLine($"An example of overflow: {what}");*/
+
+// Step 4
+decimal min = decimal.MinValue;
+decimal max = decimal.MaxValue;
+Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
