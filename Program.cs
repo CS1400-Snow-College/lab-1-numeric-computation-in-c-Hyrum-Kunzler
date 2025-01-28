08eb92214527e4130ad231cabdc4fbb8077320cc
Hyrum-Kunzler <Hyrum.kunzler@students.snow.edu>
2025-01-28 16:34:19 -0700

    The actual 4th step

--- Program.cs
+++ Program.cs
@@ -31,7 +31,8 @@ Console.WriteLine($"The range of integers is {min} to {max}");
 int what = max + 3;
 Console.WriteLine($"An example of overflow: {what}");*/
 
-/* Step 4
+
+/* Step 5 actually
 decimal min = decimal.MinValue;
 decimal max = decimal.MaxValue;
 Console.WriteLine($"The range of the decimal type is {min} to {max}");
@@ -44,7 +45,29 @@ decimal c = 1.0M;
 decimal d = 3.0M;
 Console.WriteLine(c / d);*/
 
-// Step 5
+/* Step 6 actually
 double radius = 2.50;
 double area = Math.PI * radius * radius;
-Console.WriteLine(area);
+Console.WriteLine(area);*/
+
+// Step 4
+// Accidentally missed this step so here it is.
+//double a = 5;
+//double b = 4;
+//double c = 2;
+//double d = (a + b) / c;
+//Console.WriteLine(d);
+
+
+double a = 19;
+double b = 23;
+double c = 8;
+double d = (a + b) / c;
+Console.WriteLine(d);
+
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
