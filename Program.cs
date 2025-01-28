a70c0819f253b72cd52878285cc2b8bf615943cd
Hyrum-Kunzler <Hyrum.kunzler@students.snow.edu>
2025-01-28 15:12:58 -0700

    2nd Step

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
-﻿int a = 18;
+﻿/* Step 1
+int a = 18;
 int b = 6;
 //int c = a + b;
 //int c = a - b;
 //int c = a * b;
 int c = a / b;
-Console.WriteLine(c);
+Console.WriteLine(c);*/
+
+// Step 2
+int a = 5;
+int b = 4;
+int c = 2;
+//int d = a + b * c;
+//int d = (a + b) * c;
+//int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+int d = (a + b) / c;
+Console.WriteLine(d);
